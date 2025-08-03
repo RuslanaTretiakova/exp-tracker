3bfbe300d94392d66472ca5d88e4c773976513a7
Ruslana <tretiakovaruslana7@gmail.com>
2025-08-03 18:33:18 +0200

    feat: update default UI layout

--- src/app/layout/DefaultLayout.vue
+++ src/app/layout/DefaultLayout.vue
@@ -1,5 +1,32 @@
 <template>
-  <main>
-    <h1>Hello</h1>
-  </main>
+  <div class="layout">
+    <AppTopbar />
+    <Toast />
+    <Splitter>
+      <SplitterPanel :size="25">
+        <ExperimentTable v-if="Object.keys(parsed).length" :data="parsed" />
+        <div v-else>
+          <Skeleton height="100vh" />
+        </div>
+      </SplitterPanel>
+
+      <SplitterPanel :size="75"><div></div></SplitterPanel>
+    </Splitter>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Skeleton from 'primevue/skeleton'
+import Splitter from 'primevue/splitter'
+import { Toast } from 'primevue'
+import SplitterPanel from 'primevue/splitterpanel'
+
+import { useExperimentStore } from '../../app/store/experimentStore'
+import { storeToRefs } from 'pinia'
+
+import AppTopbar from '../../components/AppTopbar.vue'
+import ExperimentTable from '../../components/experiment/ExperimentTable.vue'
+
+const experimentStore = useExperimentStore()
+const { parsed } = storeToRefs(experimentStore)
+</script>
