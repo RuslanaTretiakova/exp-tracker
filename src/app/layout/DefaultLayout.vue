b92fc80be2c03c067f60cac098302ae5f55c880b
Ruslana <tretiakovaruslana7@gmail.com>
2025-08-04 00:39:34 +0200

    feat: update layout structure and responsiveness

--- src/app/layout/DefaultLayout.vue
+++ src/app/layout/DefaultLayout.vue
@@ -1,32 +1,65 @@
 <template>
   <div class="layout">
-    <AppTopbar />
+    <AppTopbar @compare="showComparison = true" />
     <Toast />
-    <Splitter>
-      <SplitterPanel :size="25">
-        <ExperimentTable v-if="Object.keys(parsed).length" :data="parsed" />
-        <div v-else>
+
+    <Splitter :layout="splitterLayout">
+      <SplitterPanel :size="30">
+        <template v-if="hasParsedData">
+          <ExperimentTable />
+        </template>
+        <template v-else>
           <Skeleton height="100vh" />
-        </div>
+        </template>
       </SplitterPanel>
 
-      <SplitterPanel :size="75"><div></div></SplitterPanel>
+      <SplitterPanel :size="75">
+        <CompareMetrics v-if="showComparison" @close="showComparison = false" />
+        <AllExperimentsChart v-else />
+      </SplitterPanel>
     </Splitter>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
+import { storeToRefs } from 'pinia'
+
 import Skeleton from 'primevue/skeleton'
 import Splitter from 'primevue/splitter'
-import { Toast } from 'primevue'
 import SplitterPanel from 'primevue/splitterpanel'
+import { Toast } from 'primevue'
 
 import { useExperimentStore } from '../../app/store/experimentStore'
-import { storeToRefs } from 'pinia'
 
 import AppTopbar from '../../components/AppTopbar.vue'
 import ExperimentTable from '../../components/experiment/ExperimentTable.vue'
+import AllExperimentsChart from '../../components/charts/AllExperimentsChart.vue'
+import CompareMetrics from '../../components/charts/CompareMetrics.vue'
+import { useSplitterLayout } from '../../utils/hooks/useSplitterLayout'
 
 const experimentStore = useExperimentStore()
 const { parsed } = storeToRefs(experimentStore)
+
+const hasParsedData = computed(() => Object.keys(parsed.value).length > 0)
+const showComparison = ref(false)
+
+const { splitterLayout } = useSplitterLayout()
 </script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.layout :deep(.p-splitter) {
+  flex: 1;
+  height: 100%;
+}
+
+.layout :deep(.p-splitter-panel) {
+  overflow: auto;
+}
+</style>
